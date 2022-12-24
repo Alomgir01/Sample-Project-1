c1e50f30f9ce0c3158026e597ec5422255ddc4dd
AlomgirH <102249051+Alomgir01@users.noreply.github.com>
2022-12-24 14:29:45 +0600

    Add files via upload

---
+++ decompose with function.c
@@ -0,0 +1,22 @@
+/*Program-decompose with function.
+Author: MD ALOMGIR HOSSAIN
+ID-2022-1-60-125
+date: 16 April,2022*/
+
+#include<stdio.h>
+void decomopose(double x, long int_part, double frac_part)
+{
+    int_part=(long) x;
+    frac_part= x-int_part;
+}
+int main(void)
+{
+    double x, frac_part;
+    long int_part;
+    printf("Enter a \'double\' value: ");
+    scanf("%lf",&x);
+    printf("\'int\' part is: %d\n",(int)x);
+    printf("\'frac\' part is: %g",x-(int)x);
+    getch();
+}
+
